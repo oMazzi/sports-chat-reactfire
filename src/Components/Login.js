ec50586b3152950221bb48ba8c4a2a33409dc54b
Will <willian.maziero@hotmail.com>
2023-05-28 19:35:02 -0400

    show password

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -5,12 +5,14 @@ import { Link } from 'react-router-dom';
 import { auth } from '../firebase';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
+import { RiEyeLine, RiEyeOffLine } from 'react-icons/ri';
 
 const Login = () => {
   const [user] = useAuthState(auth);
   const [email, setEmail] = React.useState('');
   const [password, setPassword] = React.useState('');
   const [error, setError] = React.useState('');
+  const [showPassword, setShowPassword] = React.useState(false);
 
   React.useEffect(() => {
     if (user) {
@@ -33,6 +35,11 @@ const Login = () => {
     }
   };
 
+  const togglePasswordVisibility = (e) => {
+    e.preventDefault();
+    setShowPassword(!showPassword);
+  };
+
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Sign in</h1>
@@ -47,11 +54,19 @@ const Login = () => {
           value={email}
           onChange={(e) => setEmail(e.target.value)}
         />
-        <label label="Password:" className={styles.label} htmlFor="password">
-          Password:
-        </label>
+        <div className={styles.passwordInputContainer}>
+          <label label="Password:" className={styles.label} htmlFor="password">
+            Password:
+          </label>
+          <button
+            className={styles.passwordToggle}
+            onClick={togglePasswordVisibility}
+          >
+            {showPassword ? <RiEyeOffLine /> : <RiEyeLine />}
+          </button>
+        </div>
         <input
-          type="password"
+          type={showPassword ? 'text' : 'password'}
           id="password"
           className={styles.input}
           value={password}
